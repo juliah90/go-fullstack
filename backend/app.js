afa99c559a4caad7198fc661f0f25d16a24c1d07
juliah90 <137844881+juliah90@users.noreply.github.com>
2024-02-15 09:24:34 -0800

    thing.js added, current 404

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,61 @@
+//MONGODB PASSWORD - mongodbfullstackjh
+//MONGODB CONNECTION: mongodb+srv://juliaann1990:<password>@cluster0.z1o3rfo.mongodb.net/?retryWrites=true&w=majority
 const express = require('express');
+const mongoose = require('mongoose');
+
+const Thing = require('./models/thing');
 
 const app = express();
 
+mongoose.connect('mongodb+srv://juliaann1990:mongodbfullstackjh@cluster0.z1o3rfo.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => {
+        console.log('Successfully connected to mongodb atlas');
+    })
+    .catch((error) => {
+        console.log('Unable to connect to mongodb atlas');
+        console.error(error);
+    })
+
+app.use(express.json());
+
 app.use((req, res, next) => {
-    console.log('Request Received')
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
-app.use((req, res, next) => {
-    res.status(201);
-    next();
+app.post('/api/stuff', (req, res, next) => {
+    const thing = new Thing({
+        title: req.body.title,
+        description: req.body.description,
+        imageUrl: req.body.imageUrl,
+        price: req.body.price,
+        userId: req.body.userId
+    });
+    thing.save().then(
+        () => {
+            res.status(201).json({
+                message: 'Post saved successfully!'
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
 });
 
-app.use((req, res, next) => {
-    res.json({ message: 'Your request was successful' });
-    next();
-});
-
-app.use((req, res, next)=>{
-    console.log('Response sent successfully');
+app.get('/api/stuff', (req, res, next) => {
+    thing.find().then((things) => {
+        res.status(200).json(things);
+    }).catch((error) => {
+        res.status(400).json({
+            error: error
+        });
+    })
 });
 
 module.exports = app;
